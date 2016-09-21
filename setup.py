3954cf7c12954e003d4e22e2b0822f4c3264b22c
TApplencourt <tapplencourt@gmail.com>
2016-09-21 12:20:26 +0200

    remove s

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, Extension
 
-module = Extension('gz2ar',
+gz2ar = "./zezfio/io/gz2ar"
+module = Extension("gz2ar",
                     libraries = ['z'],
-                    sources = ['./zezfio/io/gz2ar.c'])
+                    sources = ['%s.c'%gz2ar])
 
 setup(
   name = 'zezfio',
-  packages = ['zezfio'],
-  scripts = ["zlegacy2json.py", "zfang.py", "zserver.py"],
-  version = '0.8',
+  packages = ['zezfio','zezfio/template','zezfio/io','zezfio/lib'],
+  scripts = ["zlegacy2json", "zfang", "zserver"],
+  version = '0.9.1',
   description = 'The lovely client/server version of Ezfio',
   author = 'Thomas applencourt',
   author_email = 'applencourt@irsamc.ups-tlse.fr',
